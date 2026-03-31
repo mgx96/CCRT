2cfe5bb88b586533bab1537e454cb6af912fce01
Malek Sharabi <maleksharabi@Maleks-MacBook-Pro.local>
2026-03-31 03:49:45 +0200

    created the vault contract

---
+++ src/Vault.sol
@@ -0,0 +1,34 @@
+// Layout of Contract:
+// license
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+contract Vault {
+    address private immutable i_rebaseToken;
+
+    constructor(address _rebaseToken) {
+        i_rebaseToken = _rebaseToken;
+    }
+}
