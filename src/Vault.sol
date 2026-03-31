0d52b715cc80b671d9892daf971b0a8202b8b3b1
Malek Sharabi <maleksharabi@Maleks-MacBook-Pro.local>
2026-03-31 04:46:17 +0200

    added natspec and a redeem function to the vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -28,9 +28,12 @@ pragma solidity ^0.8.30;
 import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
 contract Vault {
+    error Vault__RedeemFailed();
+
     IRebaseToken private immutable i_rebaseToken;
 
     event Deposit(address indexed user, uint256 amount);
+    event Redeem(address indexed user, uint256 amount);
 
     constructor(IRebaseToken _rebaseToken) {
         i_rebaseToken = _rebaseToken;
@@ -41,6 +44,15 @@ contract Vault {
     function deposit() external payable {
         i_rebaseToken.mint(msg.sender, msg.value);
         emit Deposit(msg.sender, msg.value);
+    }
+
+    function redeem(uint256 _amount) external {
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender, _amount);
     }
 
     function getRebaseTokenAddress() external view returns (address) {
