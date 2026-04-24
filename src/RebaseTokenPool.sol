0224e07af53263df8091a0412d1962b5fe2b1f51
Malek Sharabi <maleksharabi@Mac.lan>
2026-04-24 15:06:15 +0200

    added a rebase token pool contract

---
+++ src/RebaseTokenPool.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import {TokenPool} from "../lib/chainlink-ccip/chains/evm/contracts/pools/TokenPool.sol";
+
+contract RebaseTokenPool is TokenPool {
+	
+}
