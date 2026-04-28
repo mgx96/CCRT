05a81f4821d93866e9b084e2f538ae6eddd5874c
Malek Sharabi <maleksharabi@Mac.lan>
2026-04-28 10:49:56 +0200

    sepolia and arb-sepolia rebase token tests

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -10,18 +10,35 @@ import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {CCIPLocalSimulatorFork} from "../lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 
 contract CrossChainTest is Test {
-    RebaseToken private rebaseToken;
-    CCIPLocalSimulatorFork private ccipLocalSimulatorFork;
-    uint256 private sepoliaFork;
-    uint256 private arbSepoliaFork;
+    RebaseToken sepoliaToken;
+    RebaseToken arbSepoliaToken;
+
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+
+    uint256 sepoliaFork;
+    uint256 arbSepoliaFork;
+    address owner = makeAddr("owner");
+
+    Vault vault;
 
     function setUp() public {
-        rebaseToken = new RebaseToken();
         ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
 
         sepoliaFork = vm.createSelectFork("sepolia");
         arbSepoliaFork = vm.createtFork("arb-sepolia");
 
         vm.makePersistent(address(ccipLocalSimulatorFork));
+
+        // Deploy and configure on Sepolia
+        vm.startPrank(owner);
+        sepoliaToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(sepoliaToken)));
+        vm.stopPrank();
+
+        // Deploy and configure on Arbitrum Sepolia
+        vm.selectFork(arbSepoliaFork);
+        vm.startPrank(owner);
+        arbSepoliaToken = new RebaseToken();
+        vm.stopPrank();
     }
 }
