f8b68520c2235c826fcbef0ac4534c84ea85d3f3
Malek Sharabi <maleksharabi@Maleks-MacBook-Pro.local>
2026-05-05 13:12:19 +0200

    token and pool deployment script finalized

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,20 +4,25 @@ pragma solidity ^0.8.30;
 
 import {Script} from "forge-std/Script.sol";
 import {Vault} from "../src/Vault.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {CCIPLocalSimulatorFork, Register} from "../lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
-import {IERC20} from "../lib/chainlink-local/src/vendor/openzeppelin-solidity/v5.0.2/contracts/interfaces/IERC20.sol";
-import {RegisterModuleOwnerCustom} from "../lib/chainlink-local/lib/chainlink-ccip/chains/evm/contracts/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {IERC20} from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
+import {RegistryModuleOwnerCustom} from "../lib/chainlink-local/lib/chainlink-ccip/chains/evm/contracts/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {TokenAdminRegistry} from "../lib/chainlink-local/lib/chainlink-ccip/chains/evm/contracts/tokenAdminRegistry/TokenAdminRegistry.sol";
 
 contract TokenAndPoolDeployer is Script {
     function run() public returns (RebaseToken token, RebaseTokenPool pool) {
         vm.startBroadcast();
-        Register.NetworkDetails networkDetails = Register.getNetworkDetails(block.chainid);
         CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
         token = new RebaseToken();
         pool = new RebaseTokenPool(IERC20(address(token)), new address[](0), networkDetails.rmnProxyAddress, networkDetails.routerAddress);
         token.grantMintAndBurnRole(address(pool));
+        RegistryModuleOwnerCustom(networkDetails.tokenAdminRegistryAddress).registerAdminViaOwner(address(token));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(token));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).setPool(address(token), address(pool));
         vm.stopBroadcast();
     }
 }
